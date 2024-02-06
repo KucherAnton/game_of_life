33e54fbe1fc0468becf142efb328a1188c3359a0
Антон Кучер <kapetrovi4@gmail.com>
2024-02-06 18:24:44 +0300

    playing with colors

--- sketch.js
+++ sketch.js
@@ -1,7 +1,35 @@
+let golShader;
+
+let prevFrame;
+
+function preload() {
+	golShader = loadShader('gol.vert', 'gol.frag');
+}
+
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(windowWidth, windowWidth - 799, WEBGL);
+	pixelDensity(1);
+	noSmooth();
+	prevFrame = createGraphics(width, height);
+	prevFrame.pixelDensity(1);
+	prevFrame.noSmooth();
+	background(0);
+	stroke(255);
+	shader(golShader);
+	golShader.setUniform('normalRes', [1.0 / width, 1.0 / height]);
 }
 
 function draw() {
-  background(220);
+	fill(255, 0, 0);
+	if (mouseIsPressed) {
+		line(
+			pmouseX - width / 2,
+			pmouseY - height / 2,
+			mouseX - width / 2,
+			mouseY - height / 2
+		);
+	}
+	prevFrame.image(get(), 0, 0);
+	golShader.setUniform('tex', prevFrame);
+	rect(-width / 2, -height / 2, width, height);
 }
